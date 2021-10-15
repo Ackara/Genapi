0e4eec0334331e73c1f64dcedfc4eb8b3e6120da
Ackara <ackara.dev@outlook.com>
2021-10-15 12:54:49 -0400

    add projects

---
+++ tests/Genapi.MSTest/Tests/SerializationTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tekcari.Genapi.Tests
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        [TestMethod]
+        public void Method1()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
